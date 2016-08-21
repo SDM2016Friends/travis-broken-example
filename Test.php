144cea43f49ea8ee8307d232727d30ff1d83e570
Isabella930514 <Isabella930514@gmail.com>
2016-08-21 17:45:13 +0800

    Update Test.php

--- Test.php
+++ Test.php
@@ -1,10 +1,3 @@
-<?php
-
-class Test extends PHPUnit_Framework_TestCase
-{
-	public function testOnePlusOne() {
-		$this->assertEquals(1+1,1);
-  	}
-}
-
-?>
+$ git add -A
+$ git commit -m 'Testing Travis CI: fixing the build'
+$ git push
